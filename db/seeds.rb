159e24fcce3dd97be84eda3c23cc79bef463f01a
AbdoNJ <mr.nejjari.abdelhafid@gmail.com>
2023-05-06 12:05:20 +0100

    Remove some code for cleaner and optimized code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+"Hello there! Welcome to our app. We hope you find it useful and enjoyable!"
+"Greetings and salutations! We're thrilled to have you on board."
+"Hey, it's great to see you here! We're excited to show you what we've got."
+"Hiya! Thanks for checking us out. We think you're going to like what you find."
+"Good day to you! We're delighted to have the opportunity to serve you."
